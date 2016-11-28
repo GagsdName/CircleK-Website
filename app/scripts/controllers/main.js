4ecf23b500a173b1e9fe0f4d7c3c1e1f3a44d152
singhgag <singhgag@indiana.edu>
2016-11-28 12:02:46 -0500

    Initial generator Check-in
    Skeleton code to get started with

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name circleKApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the circleKApp
+ */
+angular.module('circleKApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
